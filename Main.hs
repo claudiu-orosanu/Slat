391079e2569961e2c0a0f21589a692e3008f84d7
Claudiu Orosanu <orosanu.claudiu@gmail.com>
2019-06-01 12:28:14 +0300

    Build small chat (private, public messages between users)

--- Main.hs
+++ Main.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
 import Network (withSocketsDo, listenOn, PortID(..), accept)
 import Text.Printf (printf, hPrintf)
-import Control.Monad (forever)
+import Control.Monad
 import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Concurrent.Async
@@ -10,57 +12,173 @@ import System.IO (Handle, hSetBuffering, BufferMode(..), hGetLine, hClose, hPutS
 				  hSetNewlineMode, universalNewlineMode)
 import Control.Exception (finally)
 
+import qualified Data.Map as Map
+import Data.Map (Map)
+
 port = 2233 :: Int
 
+main :: IO ()
 main = withSocketsDo $ do
+	server <- newServer
 	sock <- listenOn (PortNumber (fromIntegral port))
 	printf "Listening on port %d\n" port
 	factor <- atomically $ newTVar 2
 	forever $ do
 		(handle, host, port) <- accept sock
 		printf "Accepted connection from %s:%s\n" host (show port)
-		forkIO $ (talk handle factor) `finally` (hClose handle)
+		forkIO $ (talk handle server) `finally` (hClose handle)
+
+
+--------------- Data types ---------------
+
+------- Client Data -------
+
+type ClientName = String
+
+data Client = Client {
+	clientName :: ClientName,
+	clientHandle :: Handle,
+	clientKicked :: TVar (Maybe String),
+	clientSendChan :: TChan Message
+}
+
+data Message =
+	Notice String | -- message from server
+	Tell ClientName String | -- private message from other cleint
+	Broadcast ClientName String | -- public message from other client
+	Command String -- command sent by client 
+
+
+newClient :: ClientName -> Handle -> STM Client
+newClient name handle = do
+	kicked <- newTVar Nothing
+	chan <- newTChan
+	return Client {
+		clientName = name,
+		clientHandle = handle,
+		clientKicked = kicked,
+		clientSendChan = chan
+	}
+
+sendMessage :: Client -> Message -> STM ()
+sendMessage Client{..} msg = do 
+	writeTChan clientSendChan msg
+
+------- Server Data -------
+
+data Server = Server {
+	clients :: TVar (Map ClientName Client)
+}
+
+newServer :: IO Server
+newServer = do
+	c <- newTVarIO Map.empty
+	return Server { clients = c }
+
+broadcast :: Server -> Message -> STM ()
+broadcast Server{..} msg = do
+	clientMap <- readTVar clients
+	mapM_ (\client -> sendMessage client msg) (Map.elems clientMap)
+
+------------------------------------------------------------
+
+validateAddClient :: Server -> ClientName -> Handle -> IO (Maybe Client)
+validateAddClient server@Server{..} clientName handle = atomically $ do
+	clientMap <- readTVar clients
+	if Map.member clientName clientMap
+		then return Nothing
+		else do
+			client <- newClient clientName handle
+			writeTVar clients $ Map.insert clientName client clientMap
+			broadcast server $ Notice (clientName ++ " joined")
+			return (Just client)
+
+removeClient :: Server -> ClientName -> IO ()
+removeClient server@Server{..} clientName = atomically $ do
+	modifyTVar' clients $ Map.delete clientName
+	broadcast server $ Notice (clientName ++ " left")
+
+sendTo :: Server -> ClientName -> Message -> STM Bool
+sendTo srv@Server{..} clientName msg = do
+	clientMap <- readTVar clients
+	case Map.lookup clientName clientMap of
+		Nothing -> return False
+		Just client -> sendMessage client msg >> return True
+
+tell :: Server -> Client -> ClientName -> String -> IO ()
+tell srv@Server{..} client@Client{..} who msg = do
+	ok <- atomically $ sendTo srv who (Tell clientName msg)
+	if ok
+		then return ()
+		else hPutStrLn clientHandle (who ++ " is not connected")
+
 
 -- Talk to the client
-talk :: Handle -> TVar Integer -> IO ()
-talk handle factor = do
+talk :: Handle -> Server -> IO ()
+talk handle server@Server{..} = do
 	hSetBuffering handle LineBuffering
   	hSetNewlineMode handle universalNewlineMode
-  	chan <- atomically newTChan
-  	race (server handle factor chan) (receive handle chan)
-  	return ()
+  	readName
+  	where
+  		readName = do
+  			hPutStrLn handle "Enter your name:"
+  			name <- hGetLine handle
+  			if null name
+  				then readName
+  				else do
+  					ok <- validateAddClient server name handle
+  					case ok of 
+  						Nothing -> do
+  							hPutStrLn handle "Name already in use. Please enter another name."
+  							readName
+  						Just client -> do
+  							runClient server client `finally` removeClient server name
 
-receive :: Handle -> TChan String -> IO ()
-receive handle chan = forever $ do 
-	line <- hGetLine handle
-	atomically $ writeTChan chan line
+runClient :: Server -> Client -> IO ()
+runClient srv@Server{..} client@Client{..} = do
+	race (server srv client) (receive client)
+	return ()
 
-server :: Handle -> TVar Integer -> TChan String -> IO()
-server handle factor chan = do 
-	currFactor <- atomically $ readTVar factor
-	hPrintf handle "Current factor: %d\n" currFactor
-	loop currFactor
-	where 
-		loop f = do
-			action <- atomically $ do
-				f' <- readTVar factor
-				if (f /= f')
-					then return (newfactor f')
-					else do
-						line <- readTChan chan
-						return (command f line)
-			action
+receive :: Client -> IO ()
+receive client@Client{..} = forever $ do 
+	msg <- hGetLine clientHandle
+	atomically $ sendMessage client (Command msg)
 
-		newfactor f = do
-			hPrintf handle "new factor: %d\n" f
-			loop f
+server :: Server -> Client -> IO ()
+server srv@Server{..} client@Client{..} = join $ atomically $ do
+	kicked <- readTVar clientKicked
+	case kicked of
+		Just reason -> return $ hPutStrLn clientHandle $
+			"You have been kicked for: " ++ reason
+		Nothing -> do
+			msg <- readTChan clientSendChan
+			return $ do
+				continue <- handleMessage srv client msg
+				when continue $ server srv client
 
-		command f line =
-			case line of
-				"end" -> hPutStrLn handle ("Thank you for using Slat")
-				'*':s -> do
-					atomically $ writeTVar factor (read s :: Integer)
-					loop f
+handleMessage :: Server -> Client -> Message -> IO Bool
+handleMessage srv@Server{..} client@Client{..} message =
+	case message of
+		Notice msg -> output $ "!! " ++ msg ++ " !!"
+		Tell name msg -> output $ "*" ++ name ++"*: " ++ msg
+		Broadcast name msg -> output $ "<" ++ name ++">: " ++ msg
+		Command msg ->
+			case words msg of
+				-- ["/kick", who] -> do
+				-- 	atomically $ kick server who clientName
+				-- 	return True
+				"/tell" : who : what -> do
+					tell srv client who (unwords what)
+					return True
+				["/quit"] -> do
+					hPutStrLn clientHandle "Thank you for using Slat!"
+					return False
+				('/':_):_ -> do
+					hPutStrLn clientHandle $ "Unrecognized command: " ++ msg
+					return True
 				_ -> do
-					hPutStrLn handle (show (f * (read line :: Integer)))
-					loop f
+					atomically $ broadcast srv $ Broadcast clientName msg
+					return True
+	where
+		output s = do hPutStrLn clientHandle s; return True
+
